2d6306dfa1f3a0d922364d5b4b70810857ddffb6
cacaocow <19597231+Cacaocow@users.noreply.github.com>
2020-02-17 13:01:03 +0100

    :tada: Setup project

---
+++ src/main/java/com/cacaocow/mvcexample/Main.java
@@ -0,0 +1,15 @@
+package com.cacaocow.mvcexample;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+public class Main {
+
+    private static Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String... args) throws IOException {
+        LOG.info("Starting MVC-Todo Example");
+    }
+}
